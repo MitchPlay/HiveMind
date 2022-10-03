57097e1f25fe6ec3cd47dfebb688af47b46e4a74
Enderdraak <Endedraak@gmail.com>
2022-10-03 16:54:55 +0200

    Added settings-updates.lua
    Update some modded settings so it can be a fun experience

---
+++ Hive_Mind_MitchPlay_0.1.7/settings-updates.lua
@@ -0,0 +1,10 @@
+
+if mods["ArmouredBiters"] then
+    data.raw["ab-enable-nest"].default_value = true
+end
+if mods["Cold_biters"] then
+    data.raw["cb-disable-mother"].default_value = false
+end
+if mods["Explosive_biters"] then
+    data.raw["eb-disable-mother"].default_value = false
+end
