267d627d282041607ba16932b604b927f3907528
Enderdraak <Endedraak@gmail.com>
2022-10-03 17:55:11 +0200

    more fixed for issues

--- Hive_Mind_MitchPlay_0.1.7/settings-updates.lua
+++ Hive_Mind_MitchPlay_0.1.7/settings-updates.lua
@@ -3,8 +3,8 @@ if mods["ArmouredBiters"] then
     data.raw["bool-setting"]["ab-enable-nest"].default_value = true
 end
 if mods["Cold_biters"] then
-    data.raw["bool-setting"]["cb-disable-mother"].default_value = false
+    data.raw["bool-setting"]["cb-disable-mother"].default_value = true
 end
 if mods["Explosive_biters"] then
-    data.raw["bool-setting"]["eb-disable-mother"].default_value = false
+    data.raw["bool-setting"]["eb-disable-mother"].default_value = true
 end
