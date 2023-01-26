ae183dba0f43ca25e883acfd6b03ecb7f1dc15e7
Enderdraak <Endedraak@gmail.com>
2023-01-26 20:08:11 +0100

    Fix for the newest space exploration
    used SE building extra pack denier

--- Hive_Mind_MitchPlay_0.2.6/data.lua
+++ Hive_Mind_MitchPlay_0.2.6/data.lua
@@ -4,3 +4,11 @@ require("data/entities/entities")
 require("data/technologies/technologies")
 require("data/tiles/creep")
 require("data/nerfs/nerfs")
+
+if se_prodecural_tech_exclusions then 
+    table.insert(se_prodecural_tech_exclusions, "hivemind-")
+    table.insert(se_prodecural_tech_exclusions, "biter-melee-damage")
+    table.insert(se_prodecural_tech_exclusions, "spitter-biological-damage")
+    table.insert(se_prodecural_tech_exclusions, "worm-biological-damage")
+    table.insert(se_prodecural_tech_exclusions, "popcap")
+end
