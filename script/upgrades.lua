fd431ecce3c3d117833122344a6221368f8c53d3
Your1stComrade <124912022+Your1stComrade@users.noreply.github.com>
2023-11-13 23:20:20 -0800

    and we agreed you'd be nice :/

--- script/upgrades.lua
+++ script/upgrades.lua
@@ -6,32 +6,47 @@ local unit_deployment = require("script/unit_deployment")
 local on_marked_for_upgrade = function(event)
   local entity = event.entity
   local target = event.target
-  local stored_pollution = shared.required_pollution[entity.name]
-  
-  local required_pollution = shared.required_pollution[target.name]
 
-  local difference = required_pollution - stored_pollution
+  local can_upgrade = false
 
-  if difference <= 0 then
-    entity.surface.create_entity{
-      name = target.name, 
-      position = entity.position, 
-      direction = event.direction,
-      force = entity.force
-    }
-    entity.destroy()
+  if get_needs_technology(ghost_name) then
+    if entity.force.technologies[get_needs_technology(ghost_name)].researched then
+      game.print("has tech")
+      can_upgrade = true
+    end
   else
-    local ent = entity.surface.create_entity{
-      name = "entity-ghost", 
-      inner_name = target.name,
-      position = entity.position, 
-      direction = event.direction,
-      force = entity.force
-    }
+    game.print("no tech required")
+    can_upgrade = true
+  end
 
-    register_ghost_built(ent, event.player_index, stored_pollution)
+  if can_upgrade then
+    local stored_pollution = shared.required_pollution[entity.name]
+  
+    local required_pollution = shared.required_pollution[target.name]
 
-    entity.destroy()
+    local difference = required_pollution - stored_pollution
+
+    if difference <= 0 then
+      entity.surface.create_entity{
+        name = target.name, 
+        position = entity.position, 
+        direction = event.direction,
+        force = entity.force
+      }
+      entity.destroy()
+    else
+      local ent = entity.surface.create_entity{
+        name = "entity-ghost", 
+        inner_name = target.name,
+        position = entity.position, 
+        direction = event.direction,
+        force = entity.force
+      }
+
+      register_ghost_built(ent, event.player_index, stored_pollution)
+
+      entity.destroy()
+    end
   end
 end
 
